aea216051c8840c9a5c8f5a1a3451cf64f08743f
Jack C. Lloyd <jc.lloyd@hotmail.co.uk>
2021-11-05 15:29:29 +0000

    Code Review
    General code review and refactor, removing redundant/inconsistent syntax whilst improving human-readability.

--- src/App.js
+++ src/App.js
@@ -2,93 +2,78 @@ import './App.css';
 import Geolocation from 'react-native-geolocation-service';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import  Button from '@mui/material/Button';
-// import DeviceInfo from 'react-native-device-info';
+import Button from '@mui/material/Button';
 
-
-const uri = 'https://geoloc-ase-api.azurewebsites.net/';
+const databaseURL = 'https://geolocation-db.com/json/';
+const serverURL = 'https://geoloc-ase-api.azurewebsites.net/';
 const path = 'api/upload';
-const headers = {headers :{ token: 12344321}};
-
-
-
-function App() {
-  const [lat, setLocLat] = useState(null);
-  const [long, setLocLong] = useState(null); 
-  const [showLocation, setShowLocation] = useState(false);
-  const [showId, setShowId] = useState(false);
-  const [ip, setIP] = useState('');
-  const [showMode, setMode] = useState(false);
-  
-  const getData = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/')
-    console.log(res.data);
-    setIP(res.data.IPv4)
+const headers = {
+  headers : {
+    token: 12344321 // ! hardcoded
   }
+};
 
-  useEffect( () => {
-    //passing getData method to the lifecycle method
-    getData()
-
-  }, [])
-
+function App () {
+  const [ip, setIP] = useState("");
+  const [showIp, setShowIp] = useState(false);
+  const [latitude, setLatitude] = useState(null); // ! typeless
+  const [longitude, setLongitude] = useState(null); // ! typeless
+  const [showLocation, setShowLocation] = useState(false);
+  const [automaticMode, setAutomatic] = useState(false);
   
+  useEffect(() => {
+    (async function () {
+      setIP((await axios.get(databaseURL)).data.IPv4); // ! anti-pattern
+    })(); // ! self-invoke
+   }, []);
 
-  Geolocation.getCurrentPosition(function (position) {
-    console.log(position)
-    setLocLat(position.coords.latitude);
-    setLocLong(position.coords.longitude);
-    // setDeviceId(DeviceInfo.getUniqueId());
-    console.log("Latitude and Longitude is: " + lat + " : " + long); 
-    
-  
-    
+  Geolocation.getCurrentPosition ((position) => { // ! anti-pattern
+    setLatitude (position.coords.latitude);
+    setLongitude (position.coords.longitude);
   });
 
-  function SendCoordinates() {
-    const body = { uid: ip, longitude: long, latitude: lat };
-    axios.post(uri + path, body, headers).then(console.log).catch(console.error); 
+  function SendLocation() {
+    const body = { // ! duplicated
+      uid: ip, // ! misname
+      longitude: longitude,
+      latitude: latitude
+    };
+
+    axios.post(serverURL + path, body, headers).then(console.log).catch(console.error); // ! duplicated
   }
 
-  const MINUTE = 3000
-    useEffect(() => {
-      const interval = setInterval(() => {
-        var body = {
-          uid: "test",
-          longitude: long,
-          latitude: lat
-        }
-        axios.post(uri + path, body, headers).then(
-          console.log()
-        );
-  
-      }, MINUTE);
-      return () => clearInterval(interval);
-    }, [lat,long]);
+  const MILLISECONDS = 3000;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const body = { // ! duplicated
+        uid: ip, // ! misname
+        longitude: longitude,
+        latitude: latitude
+      };
+
+      if (automaticMode) {
+        axios.post(serverURL + path, body, headers).then(console.log).catch(console.error);  // ! duplicated
+      }
+    }, MILLISECONDS);
+
+    return () => clearInterval(interval);
+  }, [ip, latitude,longitude, automaticMode]);
   
   return (
     <div className="App">
       <header className="App-header">
-        { showId ? ip.toString() : "–" }   
-        <Button onClick={() => setShowId(!showId)}> {showId ? "Hide ID" : "Show ID"} </Button>
-        {showLocation ? lat + "" + long : "–"}
-        <Button onClick={() => setShowLocation(!showLocation)}> {showLocation ? "Hide Location" : "Show Location"} </Button>
-        <Button onClick={SendCoordinates}> Send Coordinates </Button>
-           
+        { showIp ? ip.toString() : "—" }
+        <Button onClick={() => setShowIp(!showIp)}>{showIp ? "Hide IP" : "Show IP"}</Button>
+        <br/>
+        {showLocation ? latitude.toString() + ' , ' + longitude.toString() : "—"}
+        <Button onClick={() => setShowLocation(!showLocation)}>{showLocation ? "Hide Location" : "Show Location"}</Button>
+        <Button onClick={SendLocation} disabled={automaticMode}>Send Location</Button>
+        <br/>
+        <Button onClick={() => setAutomatic(!automaticMode)}>{automaticMode ? "Enable Manual Mode" : "Enable Automatic Mode"}</Button>
       </header>
-
     </div>
   );
 }
 
 export default App;
-
-
-// Button at the bottom that toggles between manual and automatic sending of data. 
-// Manual mode is going to send it only if button pressed 
-// Automatic mode will also work with the button but it needs to be regularly sending (every minute) 
-
-// Move the ID button to the right-top-corner. - Yunusa
-// Send Coordinates needs to be placed somewhere else. - Callum   
-// Send Coordinates Button needs to have a function attached to it that sends the location when the "Manual/Automatic" button is set on Manual.
-// Button for switching between manual and automatic mode. 
