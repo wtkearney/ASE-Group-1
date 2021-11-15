f9cff33e23dae264214c0f337a6989dd2376d024
Will Kearney <wkearney@colby.edu>
2021-11-15 23:33:08 +0000

    Made things pretty

--- stylesheet.ts
+++ stylesheet.ts
@@ -1,6 +1,14 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+
+    backgroundContainer: {
+        flex: 1,
+        backgroundColor: '#F4EBD0',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -10,7 +18,13 @@ const styles = StyleSheet.create({
     textStyle: {
         fontSize: 20
     },
+    subtitle: {
+        color: "#122620",
+        fontSize: 15,
+        textAlign: 'left'
+    },
     title : {
+        color: "#122620",
         fontSize: 45,
         alignItems: 'center',
         justifyContent: 'center',
@@ -23,22 +37,55 @@ const styles = StyleSheet.create({
     },
     appButtonContainer: {
         elevation: 8,
-        backgroundColor: "#009688",
-        borderRadius: 20,
+        backgroundColor: "#122620",
+        borderRadius: 40,
         paddingVertical: 15,
         paddingHorizontal: 20,
       },
     appButtonText: {
         fontSize: 18,
-        color: "#fff",
+        color: "#F4EBD0",
         fontWeight: "bold",
         alignSelf: "center",
         textTransform: "uppercase"
     },
-    space: {
-        width: 20,
-        height: 20,
+
+    alreadyHaveAccountContainer: {
+        elevation: 8,
+        // backgroundColor: "#F4EBD0",
+        borderRadius: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
       },
+    
+      alreadyHaveAccountText: {
+        fontSize: 15,
+        elevation: 8,
+        borderRadius: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+      },
+
+      alreadyHaveAccountHighlightText: {
+        elevation: 8,
+        borderRadius: 20,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+      },
+
+
+    space: {
+        width: 40,
+        height: 40,
+      },
+      input: {
+        borderRadius: 10,
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      }
 });
 
 export default styles;
