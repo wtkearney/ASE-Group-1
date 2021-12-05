83641a043a42585c5cab6260846933b0e42fb888
Will Kearney <wtkearney@gmail.com>
2021-12-05 23:47:57 +0000

    Added SafeAreaProvider wrapping

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Router} from './src/routes/Router';
 import {AuthProvider} from './src/contexts/Auth';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router/>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <Router/>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 };
 
