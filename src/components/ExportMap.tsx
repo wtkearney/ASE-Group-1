e69e8a7ca0d18400af72ddedbe3aadd2eed6acc3
Will Kearney <wtkearney@gmail.com>
2021-12-05 23:47:24 +0000

    Loading displayed while map data loaded from server

--- src/components/ExportMap.tsx
+++ src/components/ExportMap.tsx
@@ -4,22 +4,31 @@ import  MapView ,{ Marker } from 'react-native-maps';
 import {useAuth} from '../contexts/Auth';
 import styles from "../../stylesheet";
 
+import {Loading} from '../components/Loading';
+
 export const ExportMap = () => {
 
   const auth = useAuth();
+
+  // const {authData} = useAuth();
+
+  if (!auth.locationData) {
+    console.log("Loading screen...")
+    return <Loading />;
+  }
     
   return (
     <MapView
       style={styles.map}
-      initialRegion={{ latitude: auth.lat,
-        longitude: auth.long,
+      initialRegion={{ latitude: auth.locationData.lat,
+        longitude: auth.locationData.long,
         latitudeDelta: 0.04,
         longitudeDelta: 0.05 }} >
 
     <Marker pinColor='#30D5C8'
       title="Current Location"
-      description={"Lat: " + auth.lat.toFixed(4) + ", Long: " + auth.long.toFixed(4)}
-      coordinate={{ latitude: auth.lat, longitude: auth.long }} />
+      description={"Lat: " + auth.locationData.lat.toFixed(4) + ", Long: " + auth.locationData.long.toFixed(4)}
+      coordinate={{ latitude: auth.locationData.lat, longitude: auth.locationData.long }} />
   </MapView>
 
   );
